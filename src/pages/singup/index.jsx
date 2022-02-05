1a17f1867907c38088bca4101fc25f7b5a497a09
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-05 19:29:18 -0300

    funcionalidade de validação

--- src/pages/singup/index.jsx
+++ src/pages/singup/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { InputEmail, InputName, InputPassword } from '../../components/inputs';
+import ErrorMessage from '../../components/errorMessage';
 import './style.scss';
 
 function Singup() {
@@ -8,6 +9,47 @@ function Singup() {
   const [emailValue, setEmailValue] = useState('');
   const [passValue, setPassValue] = useState('');
   const [repassValue, setRePassValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState({
+    errorName: false,
+    errorEmail: false,
+    errorPass: false,
+    errorRepPass: false,
+  });
+
+  function handleSingup() {
+    if (!verifyInputs()) {
+      return;
+    }
+  }
+
+  function verifyInputs() {
+    if (!nameValue) {
+      const backup = { ...errorMessage, errorName: true };
+      setErrorMessage(backup);
+      return false;
+    }
+    if (!emailValue) {
+      const backup = { ...errorMessage, errorEmail: true };
+      setErrorMessage(backup);
+      return false;
+    }
+    if (!passValue) {
+      const backup = { ...errorMessage, errorPass: true };
+      setErrorMessage(backup);
+      return false;
+    }
+    if (!repassValue) {
+      const backup = { ...errorMessage, errorRepPass: true };
+      setErrorMessage(backup);
+      return false;
+    }
+    setErrorMessage({
+      errorName: false,
+      errorPass: false,
+      errorEmail: false,
+      errorRepPass: false,
+    });
+  }
 
   return (
     <div className="Singup">
@@ -18,18 +60,26 @@ function Singup() {
           </div>
           <div className="inputs">
             <InputName state={nameValue} setState={setNameValue} />
+            {errorMessage.errorName && <ErrorMessage text="Falta o nome" />}
             <InputEmail state={emailValue} setState={setEmailValue} />
+            {errorMessage.errorEmail && <ErrorMessage text="Falta o E-mail" />}
             <InputPassword state={passValue} setState={setPassValue}>
               Senha
             </InputPassword>
+            {errorMessage.errorPass && <ErrorMessage text="Falta a senha" />}
             <InputPassword state={repassValue} setState={setRePassValue}>
               Repetir senha
             </InputPassword>
-            <button className="submit-btn">Cadastre-se</button>
+            {errorMessage.errorRepPass && (
+              <ErrorMessage text="Falta repetir a senha" />
+            )}
+            <button onClick={handleSingup} className="submit-btn">
+              Cadastre-se
+            </button>
           </div>
           <span className="redirect-singup">
             Já tem conta? faça seu login clicando
-            <NavLink to="/singup">aqui</NavLink>
+            <NavLink to="/login">aqui</NavLink>
           </span>
         </div>
       </div>
