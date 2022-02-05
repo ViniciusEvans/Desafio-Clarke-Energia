5c32b32923c9534171b7e629c55075b46aa849ca
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-05 15:40:41 -0300

    fazendo validação dos inputs e menssagem de erro

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,6 +5,31 @@ import './style.scss';
 function Login() {
   const [passwordValue, setPasswordValue] = useState('');
   const [emailValue, setEmailValue] = useState('');
+  const [error, setError] = useState({
+    emailError: false,
+    passwordError: false,
+  });
+
+  function handleLogin() {
+    if (!verifyInputs()) {
+      return;
+    }
+  }
+
+  function verifyInputs() {
+    if (!emailValue) {
+      const backup = { ...error, emailError: true };
+      setError(backup);
+      return false;
+    }
+    if (!passwordValue) {
+      const backup = { ...error, passwordError: true };
+      setError(backup);
+      return false;
+    }
+    setError({ emailError: false, passwordError: false });
+  }
+
   return (
     <div className="login">
       <div className="container-left"></div>
@@ -18,11 +43,23 @@ function Login() {
               state={emailValue}
               setState={setEmailValue}
             ></InputEmail>
+            {error.emailError && (
+              <span style={{ fontSize: '1.6rem', color: 'red' }}>
+                Precisa de email
+              </span>
+            )}
             <InputPassword
               state={passwordValue}
               setState={setPasswordValue}
             ></InputPassword>
-            <button className="submit-btn">login</button>
+            {error.passwordError && (
+              <span style={{ fontSize: '1.6rem', color: 'red' }}>
+                Precisa de senha
+              </span>
+            )}
+            <button onClick={handleLogin} className="submit-btn">
+              login
+            </button>
           </div>
           <span className="redirect-singup">
             Não é cadastrado? faça seu cadstro clicando
