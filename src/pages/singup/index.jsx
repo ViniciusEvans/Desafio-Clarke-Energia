16c6f05f46fdc372fd443a56b809e520fbd208d6
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-09 21:15:57 -0300

    feature de verificar inputs, cadastra conta e erro em tela

--- src/pages/singup/index.jsx
+++ src/pages/singup/index.jsx
@@ -1,10 +1,15 @@
-import { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import { InputEmail, InputName, InputPassword } from '../../components/inputs';
+import { useState } from 'react';
+import { useNavigate, NavLink } from 'react-router-dom';
 import ErrorMessage from '../../components/errorMessage';
+import { InputEmail, InputName, InputPassword } from '../../components/inputs';
 import './style.scss';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Singup() {
+  const navigate = useNavigate();
+  const notifyError = (message) => toast.error(message);
+  const notifySucess = (message) => toast.success(message);
   const [inputValues, setInputValues] = useState({
     nameValue: '',
     emailValue: '',
@@ -23,11 +28,11 @@ function Singup() {
     errorRepPass: false,
   });
 
-  function handleSingup() {
-    if (!verifyInputs()) {
+  async function handleSingup() {
+    if (!(await verifyInputs())) {
       return;
     }
-    if (!verifyPassword()) {
+    if (!(await verifyPassword())) {
       return;
     }
     setErrorMessage({
@@ -36,6 +41,29 @@ function Singup() {
       errorEmail: false,
       errorRepPass: false,
     });
+    try {
+      const body = {
+        name: inputValues.nameValue,
+        email: inputValues.emailValue,
+        password: inputValues.passValue,
+      };
+      const response = await fetch('http://localhost:3001/signup', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
+      if (response.status === 200) {
+        notifySucess('Cadastrado com sucesso!');
+        setTimeout(() => navigate('/login'), 1000);
+      } else {
+        notifyError(data.mensagem);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   function verifyInputs() {
@@ -98,6 +126,7 @@ function Singup() {
 
   return (
     <div className="Singup">
+      <ToastContainer />
       <div className="container-left">
         <div className="form">
           <div className="title">
