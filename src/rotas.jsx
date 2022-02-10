77328aeaaf5dbec7b9992924ecc26218ec3c3dde
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-09 21:09:00 -0300

    criando rota privada

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -1,20 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
-import GlobalAuthContext from './contexts/AuthContext/contextProvider';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Dashboard from './pages/marketplace';
 import Redirecting from './pages/redirect';
 import Singup from './pages/Singup';
+import { useLocalStorage } from 'react-use';
+import AuthProvider from './contexts/AuthContext/authProvider';
 
+function PrivateRoute({ children, authToken }) {
+  return authToken ? children : <Navigate to="/login" />;
+}
 function Rotas() {
+  const [authToken, setAuthToken, remove] = useLocalStorage('token', '');
+
   return (
-    <GlobalAuthContext value={{ msg: 'olá mundo' }}>
+    <AuthProvider value={{ authToken, setAuthToken, remove }}>
       <Routes>
         <Route path="/singup" element={<Singup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute authToken={authToken}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
         <Route path="/" element={<Redirecting />} />
       </Routes>
-    </GlobalAuthContext>
+    </AuthProvider>
   );
 }
 
